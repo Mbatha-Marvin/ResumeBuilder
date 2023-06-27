e6055bbb278de397476e097419aa09c1c77f63c8
Mbatha-Marvin <mbathamarvin597@gmail.com>
2023-06-27 11:30:37 +0300

    Database tables and models
    The following changes occured:
    	- Database session function was created
    	- Database initialization function was created
    	- Database Models and pydantic models were created

--- FastApiBackend/app/core/db.py
+++ FastApiBackend/app/core/db.py
@@ -1,2 +1,18 @@
+# db refers to database in this file
+
 from app import settings
 from sqlmodel import create_engine, SQLModel, Session
+
+
+DATABASE_URL = settings.db_connection_str
+db_connection_args = {"check_same_thread": False}
+engine = create_engine(url=DATABASE_URL, connect_args=db_connection_args, echo=True)
+
+
+def initialize_db():
+    SQLModel.metadata.create_all(bind=engine)
+
+
+def get_db_session():
+    with Session(engine) as session:
+        yield session
