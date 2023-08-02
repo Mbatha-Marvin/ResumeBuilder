07f04911f8b3a15b826549967f0b89d20786973b
robert5313 <kiptoorobert@zetech.ac.ke>
2023-08-02 20:44:59 +0300

    updated files

--- ReactFrontend/my-app/src/main.jsx
+++ ReactFrontend/my-app/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Footer from './components/Footer.jsx'
-
-
 import App from './App.jsx'
 import './App.css'
+import * as bootstrap from 'bootstrap' ;
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Footer />
   </React.StrictMode>,
 )
