7a407bc8862b44c37e3a40db3a5d46d45188be33
robert5313 <kiptoorobert@zetech.ac.ke>
2023-08-03 09:23:00 +0300

    updates of files

--- ReactFrontend/my-app/src/main.jsx
+++ ReactFrontend/my-app/src/main.jsx
@@ -11,5 +11,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
