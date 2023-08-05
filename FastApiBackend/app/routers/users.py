638b83268f655a6e2593e48838abb6e081bda18b
Mbatha-Marvin <mbathamarvin597@gmail.com>
2023-08-05 12:29:24 +0300

    Move User Business logic out of the API endpoint

--- FastApiBackend/app/routers/users.py
+++ FastApiBackend/app/routers/users.py
@@ -2,6 +2,7 @@ from fastapi import Depends, HTTPException, Query, APIRouter
 from app.core.db import get_db_session
 from typing import List
 from sqlmodel import Session, select
+from app.helpers.DataValidations.user_endpoint import UserEndpointValidations
 
 from app.core.models import UserRead, User, UserCreate, UserUpdate, UserProfileRead
 
@@ -52,8 +53,11 @@ def create_user(
     session: Session = Depends(get_db_session),
     user: UserCreate,
 ):
+    user_validation = UserEndpointValidations(session=session)
+
+    if user_validation.email_exists(email=user.email):
+        raise HTTPException(status_code=409, detail="Email already exists")
     db_user = User.from_orm(user)
-    print(db_user)
 
     session.add(db_user)
     session.commit()
@@ -69,17 +73,8 @@ def update_user(
     user_id: int,
     user: UserUpdate,
 ):
-    db_user_instance = session.get(User, user_id)
-    if not db_user_instance:
-        raise HTTPException(status_code=404, detail="User not Found")
-
-    new_user_data = user.dict(exclude_unset=True)
-    for key, value in new_user_data.items():
-        setattr(db_user_instance, key, value)
-
-    session.add(db_user_instance)
-    session.commit()
-    session.refresh(db_user_instance)
+    user_validation = UserEndpointValidations(session=session)
+    db_user_instance = user_validation.update_user(user_id=user_id, new_user=user)
 
     return db_user_instance
 
